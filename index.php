a220b9b9f9a553c26d64dae95cef8cb326493588
Dmytro Kotenko <d.kotenko@i.ua>
2017-12-20 20:39:44 +0200

    Added comments demonstration

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-Some data
-Other string with conflict resolution
-Third string
-Next line from test branch
+<?
+
+// One line comment
+
+# Other one line comment
+
+/* Multi line comment with one line */
+
+/*
+ * Multi line comment
+ * more lines
+ * For great visualisation
+ */
+
+/*
+Other
+multi line
+comment
+*/
+
+/**
+ * @var string $test
+ * @method testMethod
+ * @return array
+ */
+
+$test = 1 + 4.2;
+
+?>
+
+<h1>Result: <?= $test ?></h1>
