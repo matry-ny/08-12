5c5e0e04bc59a9877cac70ac87da04928481b8de
Dmytro Kotenko <d.kotenko@i.ua>
2018-01-22 20:56:03 +0200

    Added functions examples

---
+++ samples/functions/anonymous.php
@@ -0,0 +1,39 @@
+<?php
+
+function validate($value, $callback)
+{
+    return $callback($value);
+}
+
+$attributes = [
+    'name' => 'is_string',
+    'phone' => function ($value) {
+        $regexp = '/\+38\s\(0\d{2}\)\s\d{3}\-\d{2}\-\d{2}/';
+        return (bool)preg_match($regexp, $value);
+    }
+];
+
+$data = [
+    'name' => 'Dmytro',
+    'phone' => '+38 (050) 167-53-61',
+    'age' => 21,
+    'weight' => 66
+];
+
+foreach ($data as $key => $value) {
+    $validator = array_key_exists($key, $attributes) ? $attributes[$key] : null;
+    if (empty($validator)) {
+        continue;
+    }
+
+    $result = validate($value, $validator);
+    var_dump($result);
+}
+
+function filterInt($value)
+{
+    return is_int($value);
+}
+
+$filtered = array_filter($data, 'filterInt');
+var_dump($filtered);
---
+++ samples/functions/arguments.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(E_ALL);
+
+function isAdult($age, $country, $gender = 'male')
+{
+    $counties = [
+        'Ukraine' => 18,
+        'USA' => 21,
+        'China' => 16
+    ];
+
+    if (array_key_exists($country, $counties)) {
+        $coefficient = $gender == 'female' ? 0.99 : 1;
+        return $counties[$country] <= $age * $coefficient;
+    }
+
+    return true;
+}
+
+var_dump(isAdult(16, 'USA', 'female'), isAdult(18, 'China'));
+
+$number = 2;
+function power(&$int, $power)
+{
+    $int = pow($int, $power);
+    return true;
+}
+if (power($number, 3)) {
+    var_dump($number);
+}
+
+$array = [3, 4, 5];
+foreach ($array as &$value) {
+    $value += 1;
+}
+var_dump($array);
