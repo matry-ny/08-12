b385de3d3587a014af4a8dd471a2adc599c50d38
Dmytro Kotenko <d.kotenko@i.ua>
2018-02-19 21:03:50 +0200

    Added parsed data saving

--- parse/index.php
+++ parse/index.php
@@ -13,8 +13,10 @@ if (empty($riversTable)) {
     die('Rivers table can not be parsed');
 }
 
+$db = mysqli_connect('localhost', 'root', 'ngJxC3h8', '0812_samples');
+
 $rows = parseRows($riversTable->getElementsByTagName('tr'));
-saveRows($rows);
+saveRows($db, $rows);
 
 /**
  * @param DOMDocument $dom
@@ -83,9 +85,85 @@ function parseColumns(DOMNodeList $columns, array $headers)
     return $result;
 }
 
-function saveRows(array $rows)
+function saveRows(mysqli $db, array $rows)
 {
-//    foreach ($rows as $row) {
-//        $countries = explode(', ', $row['Countries in the drainage basin[citation needed]']);
-//    }
+    $newRiverSQL = mysqli_prepare($db, 'INSERT INTO rivers (title) VALUES (?)');
+    foreach ($rows as $row) {
+        $river = normalizeName($row['River']);
+        $riverId = getRiverId($db, $river);
+        if (empty($riverId)) {
+            mysqli_stmt_bind_param($newRiverSQL, 's', $river);
+            mysqli_stmt_execute($newRiverSQL);
+
+            $riverId = mysqli_insert_id($db);
+        }
+
+        $countries = explode(', ', $row['Countries in the drainage basin[citation needed]']);
+        foreach ($countries as &$country) {
+            $country = normalizeName($country);
+        }
+
+        $countries = createCountries($db, $countries);
+        createRelations($db, $riverId, $countries);
+    }
+}
+
+function normalizeName($name, $symbols = ['(', '['])
+{
+    foreach ($symbols as $symbol) {
+        if (($symbolPosition = mb_stripos($name, $symbol)) !== false) {
+            $name = mb_substr($name, 0, $symbolPosition);
+        }
+    }
+
+    return trim($name);
+}
+
+function getRiverId(mysqli $db, $riverName)
+{
+    $sql = mysqli_prepare($db, 'SELECT id FROM rivers WHERE title = ?');
+    mysqli_stmt_bind_param($sql, 's', $riverName);
+    mysqli_stmt_execute($sql);
+    mysqli_stmt_bind_result($sql, $id);
+    mysqli_stmt_fetch($sql);
+
+    return $id;
+}
+
+function createCountries(mysqli $db, array $countries)
+{
+    $allSql = 'SELECT id, title FROM cities WHERE title IN ("' . implode('", "', $countries) . '")';
+    $result = mysqli_query($db, $allSql);
+
+    $existedIds = [];
+    $existed = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $existedIds[] = $row['id'];
+        $existed[] = $row['title'];
+    }
+
+    $new = array_diff($countries, $existed);
+    if ($new) {
+        $newSql = 'INSERT INTO cities (title) VALUES ("' . implode('"), ("', $new) . '")';
+        mysqli_query($db, $newSql);
+
+        $sql = 'SELECT id FROM cities WHERE title IN ("' . implode('", "', $new) . '")';
+        $result = mysqli_query($db, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $existedIds[] = $row['id'];
+        }
+    }
+
+    return $existedIds;
+}
+
+function createRelations(mysqli $db, $riverId, array $countries)
+{
+    $rows = [];
+    foreach ($countries as $countryId) {
+        $rows[] = "({$riverId}, {$countryId})";
+    }
+
+    $sql = 'INSERT INTO river_to_city (river_id, city_id) VALUES ' . implode(',', $rows);
+    mysqli_query($db, $sql);
 }
